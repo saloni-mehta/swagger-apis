cf850827ecd405ece870bd18829d7a9597f79aae
saloni-mehta <32770000+saloni-mehta@users.noreply.github.com>
2017-11-01 10:50:20 +0530

    Delete Login.js

--- Login.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var Login = require('./LoginService');
-
-module.exports.getLoginDetails = function getLoginDetails (req, res, next) {
-  Login.getLoginDetails(req.swagger.params, res, next);
-};
